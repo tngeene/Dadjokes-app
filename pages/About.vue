6e17d23ca80134a76759eb86aae77057ff53ab43
Champé <tngeene27@gmail.com>
2020-01-19 16:26:10 +0300

    initial commit

---
+++ pages/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>This is a dad jokes app built with nuxt.</div>
+</template>
+
+<script>
+export default {
+    head(){
+        return {
+            title: "About",
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "Best place for corny dad jokes"
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h2>Welcome to the best dad jokes</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    head(){
+        return {
+            title: "welcome to bad dad jokes",
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "Best place for corny dad jokes"
+                }
+            ]
+        };
+    }
+};
+</script>
+
